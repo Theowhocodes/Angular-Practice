a32d9135605313bdbc5c17de7d3b3a1d027d848b
Theowhocodes <williams2theodore@gmail.com>
2023-01-18 15:06:48 -0500

    fixed compiler issue

--- Course_Project_App/src/app/app.module.ts
+++ Course_Project_App/src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeComponent } from './recipes/recipes.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeListComponent,
-    RecipeItemComponent,
     RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
     HeaderComponent,
-    RecipeComponent
+    RecipeComponent,
+    RecipeItemComponent
+  
   ],
   imports: [
     BrowserModule
