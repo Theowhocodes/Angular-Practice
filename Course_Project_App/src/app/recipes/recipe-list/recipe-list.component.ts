943b9c34cea386b277984fa327470d11b1f73813
Theowhocodes <williams2theodore@gmail.com>
2023-01-19 11:49:08 -0500

    Created a recipe model, Navbar start of adding info to components

--- Course_Project_App/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Course_Project_App/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Spaghetti', 'simple spaghetti', 'https://www.allrecipes.com/thmb/ASRzxoRrPoMLQEpczFvU7osJNF4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/21353-italian-spaghetti-sauce-with-meatballs-2x1-141-cedbb650b4264576ab923c91215ce7fc.jpg'),
+  ]; 
 
 }
